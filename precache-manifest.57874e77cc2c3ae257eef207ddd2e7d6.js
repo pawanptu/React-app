e174ca719eed4be085d981896648c4ec8516c5f0
pawan kumar <pwnpec2019@gmail.com>
2020-09-11 15:30:19 +0530

    Updates

---
+++ precache-manifest.57874e77cc2c3ae257eef207ddd2e7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6da4de5d325f8e0e792e4ba9fbcb543",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "485d48ffaaae403b6308",
+    "url": "/React-app/static/css/main.119b63d3.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/React-app/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485d48ffaaae403b6308",
+    "url": "/React-app/static/js/main.49ba7f73.chunk.js"
+  },
+  {
+    "revision": "638738765b248673ef7b",
+    "url": "/React-app/static/js/runtime-main.fc014e74.js"
+  }
+]);
